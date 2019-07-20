0384e1ade531023c9659a3aa6d4d6cb267bb2e11
Agata Musiuk <agata.musiuk@gmail.com>
2019-07-20 20:30:24 +0200

    JS updated

--- js/script.js
+++ js/script.js
@@ -2,9 +2,6 @@
 
 // DECLARING VARIABLES - REFERENCES TO HTML
 
-var paper = document.getElementById('paper');
-var rock = document.getElementById('rock');
-var scissors = document.getElementById('scissors');
 var buttons = document.querySelectorAll('.player-move');
 var output = document.getElementById('output');
 var points = document.getElementById('points');
@@ -21,9 +18,10 @@ var playerMove;
 var playerInfo;
 var compMove;
 var compInfo;
+var row;
 
-var numberChoice = function() {
-  	return Math.round(Math.random() * 3 + 1);
+var numberChoice = function () {
+	return Math.round(Math.random() * 3 + 1);
 }
 
 // PARAMS
@@ -32,6 +30,8 @@ var params = {
 	computerPoints: 0,
 	playerPoints: 0,
 	roundNumber: 0,
+	round: 0,
+	roundPoints: '',
 	progress: []
 };
 
@@ -49,6 +49,8 @@ function reset() {
 	points.innerHTML = '0 – 0';
 	rounds.innerHTML = '0';
 	output.innerHTML = '';
+	params.round = 0;
+	params.progress = [];
 }
 
 function toogleButtons(state) {
@@ -60,7 +62,7 @@ function toogleButtons(state) {
 
 window.onload = toogleButtons(true);
 
-newGameButton.addEventListener('click', function() {
+newGameButton.addEventListener('click', function () {
 	reset();
 	params.roundNumber = window.prompt('How many rounds do you want to play?');
 	if (!isNaN(params.roundNumber) && params.roundNumber !== '' && params.roundNumber !== ' ' && params.roundNumber !== null) {
@@ -87,81 +89,101 @@ function computerChoice() {
 function determiningWinner() {
 	if (playerMove == compMove) {
 		output.innerHTML = 'Chance for a rematch! You played ' + playerInfo + ' and computer played ' + compInfo + '.';
+		params.roundPoints = '0 – 0';
 	} else if (playerMove == 1 && compMove == 2 || playerMove == 2 && compMove == 3 || playerMove == 3 && compMove == 1) {
 		output.innerHTML = 'Congratulations, you won! You played ' + playerInfo + ' and computer played ' + compInfo + '.';
 		params.playerPoints++;
+		params.roundPoints = '1 – 0';
 	} else {
 		output.innerHTML = 'Sorry, you lost. You played ' + playerInfo + ' and computer played ' + compInfo + '.';
 		params.computerPoints++;
+		params.roundPoints = '0 – 1';
 	}
-	points.innerHTML = params.playerPoints + ' – ' + params.computerPoints;  
+	points.innerHTML = params.playerPoints + ' – ' + params.computerPoints;
+
+	params.progress.push({
+		round: params.round,
+		player: playerInfo,
+		computer: compInfo,
+		roundScore: params.roundPoints,
+		gameScore: params.playerPoints + ' – ' + params.computerPoints
+	});
+
 	gameOver();
 }
 
 
 // PLAYER MOVE
 
-for ( var i = 0; i < buttons.length; i++ ) {
-    
-    playerMove = buttons[i].getAttribute('data-move');
-    playerInfo = playerMove;
+for (var i = 0; i < buttons.length; i++) {
 
-    buttons[i].addEventListener('click', function() {
+	buttons[i].addEventListener('click', function () {
 
-        if(playerMove === 'paper') {
-            playerMove = 1;
-        } else if(playerMove === 'rock') {
-            playerMove = 2;
-        } else {
-            playerMove = 3;
-        }
-    
-        computerChoice();
-        determiningWinner();
+		params.round++;
+		playerInfo = event.target.getAttribute('data-move');
+
+		if (playerInfo === 'paper') {
+			playerMove = 1;
+		} else if (playerInfo === 'rock') {
+			playerMove = 2;
+		} else {
+			playerMove = 3;
+		}
+
+		computerChoice();
+		
+
+		
+
+		determiningWinner();
+
+		console.log(params.progress);
+		
 	});
-	
-	// info for the table
-	var roundInfo = {
-		roundNumber: buttons[i],
-		player: playerInfo,
-		computer: compInfo,
-		roundScore: i,
-		gameScore: params.playerPoints + ' – ' + params.computerPoints
-	}
-
-	params.progress.push( roundInfo );
 }
 
 
 // MODALE
 
+function showModal() {
+	event.preventDefault();
+	document.querySelector('#modal-overlay').classList.add('show');
+	document.querySelector('.modal').classList.add('show');
 
+	if (params.computerPoints > params.playerPoints) {
+		endOfGameOutput.innerHTML = 'Sorry, you lost the game. Press new game button to play again.';
+	} else if (params.computerPoints < params.playerPoints) {
+		endOfGameOutput.innerHTML = 'Congratulations! You have won the entire game. Press new game button to play again.';
+	} else {
+		endOfGameOutput.innerHTML = 'It is a draw! Press new game button to play again.';
+	}
+}
 
-function endofGameModal() { 
-	
-	var hideModal = function(event) {
-		event.preventDefault();
-		document.querySelector('#modal-overlay').classList.remove('show');
-	};
-	
+function hideModal(event) {
+	event.preventDefault();
+	document.querySelector('#modal-overlay').classList.remove('show');
+}
+
+function endofGameModal() {
+
 	var closeButtons = document.querySelectorAll('.modal .close');
-	
-	for(var i = 0; i < closeButtons.length; i++) {
+
+	for (var i = 0; i < closeButtons.length; i++) {
 		closeButtons[i].addEventListener('click', hideModal);
 	}
 
 	document.querySelector('#modal-overlay').addEventListener('click', hideModal);
-	
+
 	var modals = document.querySelectorAll('.modal');
-	
-	for(var i = 0; i < modals.length; i++) {
-		modals[i].addEventListener('click', function(event) {
+
+	for (var i = 0; i < modals.length; i++) {
+		modals[i].addEventListener('click', function (event) {
 			event.stopPropagation();
 		});
 	}
 
 	createTable();
-	
+
 }
 
 // GAMEOVER
@@ -169,42 +191,42 @@ function endofGameModal() {
 function gameOver() {
 
 	if (params.computerPoints == params.roundNumber || params.playerPoints == params.roundNumber) {
-
 		toogleButtons(true);
-		
-		function showModal() {
-			event.preventDefault();
-			document.querySelector('#modal-overlay').classList.add('show');
-
-			if (params.computerPoints > params.playerPoints) {
-				endOfGameOutput.innerHTML = 'Sorry, you lost the game. Press new game button to play again.';
-				} else if (params.computerPoints < params.playerPoints) {
-					endOfGameOutput.innerHTML = 'Congratulations! You have won the entire game. Press new game button to play again.';
-				} else {
-					endOfGameOutput.innerHTML = 'It is a draw! Press new game button to play again.';
-				}
-			}
-		}
-
-	endofGameModal();
+		endofGameModal();
+	}
 }
-
 // CREATE TABLE
 
 function createTable() {
+	console.log(params.progress);
 
-	params.progress = progress;
+	tableContent.innerHTML = '';
 
-	for( var i = 0; i < progress.length; i++ ) {
+	for (var i = 0; i < params.progress.length; i++) {
 
-		var row = tableContent.insertRow(i);
+		row = document.createElement('tr');
+		tableContent.appendChild(row);
 
-		for( roundInfo in progress ) {
-			var td = document.createElement('td');
-			td.innerHTML = progress[roundInfo];
-			row.appendChild(td);
-		}
+		var td = document.createElement('td');
+		td.innerText = params.progress[i].round;
+		row.appendChild(td);
+
+		td = document.createElement('td');
+		td.innerText = params.progress[i].player;
+		row.appendChild(td);
+
+		td = document.createElement('td');
+		td.innerText = params.progress[i].computer;
+		row.appendChild(td);
+
+		td = document.createElement('td');
+		td.innerText = params.progress[i].roundScore;
+		row.appendChild(td);
+
+		td = document.createElement('td');
+		td.innerText = params.progress[i].gameScore;
+		row.appendChild(td);
 	}
 
-	tableContent.appendChild(row);
+	showModal();
 }
